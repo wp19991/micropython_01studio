291ecc488a336304d5722ffdda6ea946bf0ff912
参考答案开心否 <983214439@qq.com>
2023-09-28 10:45:01 +0800

    增加注释

--- ports/esp32/py-drone/drivers/pm/include/pm_esplane.h
+++ ports/esp32/py-drone/drivers/pm/include/pm_esplane.h
@@ -1,28 +1,11 @@
 /**
- *    ||          ____  _ __
- * +------+      / __ )(_) /_______________ _____  ___
- * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
- * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
- *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
- *
  * ESP-Drone firmware
  *
- * Copyright 2019-2020  Espressif Systems (Shanghai)
- * Copyright (C) 2011-2012 Bitcraze AB
+ * pm_esplane.h - 电源管理驱动和函数。
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, in version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * pm_esplane.h - Power Management driver and functions.
+ * 这些函数和宏定义用于管理系统的电源，包括电池状态、充电状态、电压测量、电流测量等功能。
+ * 函数之间的调用关系在代码中是线性的，没有函数相互调用。
+ * 函数主要用于配置和监测电源管理功能，以确保系统电源的稳定和可靠。
  */
 
 #ifndef PM_H_
@@ -30,32 +13,35 @@
 
 #include "driver/adc.h"
 
+// 默认的临界低电压和超时时间
 #ifndef CRITICAL_LOW_VOLTAGE
-  #define PM_BAT_CRITICAL_LOW_VOLTAGE   3.0f
+#define PM_BAT_CRITICAL_LOW_VOLTAGE   3.0f
 #else
-  #define PM_BAT_CRITICAL_LOW_VOLTAGE   CRITICAL_LOW_VOLTAGE
+#define PM_BAT_CRITICAL_LOW_VOLTAGE   CRITICAL_LOW_VOLTAGE
 #endif
 #ifndef CRITICAL_LOW_TIMEOUT
-  #define PM_BAT_CRITICAL_LOW_TIMEOUT   M2T(1000 * 5) // 5 sec default
+#define PM_BAT_CRITICAL_LOW_TIMEOUT   M2T(1000 * 5) // 默认5秒
 #else
-  #define PM_BAT_CRITICAL_LOW_TIMEOUT   CRITICAL_LOW_TIMEOUT
+#define PM_BAT_CRITICAL_LOW_TIMEOUT   CRITICAL_LOW_TIMEOUT
 #endif
 
+// 默认的低电压和超时时间
 #ifndef LOW_VOLTAGE
-  #define PM_BAT_LOW_VOLTAGE   3.2f
+#define PM_BAT_LOW_VOLTAGE   3.2f
 #else
-  #define PM_BAT_LOW_VOLTAGE   LOW_VOLTAGE
+#define PM_BAT_LOW_VOLTAGE   LOW_VOLTAGE
 #endif
 #ifndef LOW_TIMEOUT
-  #define PM_BAT_LOW_TIMEOUT   M2T(1000 * 5) // 5 sec default
+#define PM_BAT_LOW_TIMEOUT   M2T(1000 * 5) // 默认5秒
 #else
-  #define PM_BAT_LOW_TIMEOUT   LOW_TIMEOUT
+#define PM_BAT_LOW_TIMEOUT   LOW_TIMEOUT
 #endif
 
+// 默认的系统关机超时时间
 #ifndef SYSTEM_SHUTDOWN_TIMEOUT
-  #define PM_SYSTEM_SHUTDOWN_TIMEOUT    M2T(1000 * 60 * 5) // 5 min default
+#define PM_SYSTEM_SHUTDOWN_TIMEOUT    M2T(1000 * 60 * 5) // 默认5分钟
 #else
-  #define PM_SYSTEM_SHUTDOWN_TIMEOUT    M2T(1000 * 60 * SYSTEM_SHUTDOWN_TIMEOUT)
+#define PM_SYSTEM_SHUTDOWN_TIMEOUT    M2T(1000 * 60 * SYSTEM_SHUTDOWN_TIMEOUT)
 #endif
 
 #define PM_BAT_DIVIDER                3.0f
@@ -64,12 +50,12 @@
 
 #define PM_BAT_IIR_SHIFT     8
 /**
- * Set PM_BAT_WANTED_LPF_CUTOFF_HZ to the wanted cut-off freq in Hz.
+ * 设置 PM_BAT_WANTED_LPF_CUTOFF_HZ 为所需的截止频率（单位：Hz）。
  */
 #define PM_BAT_WANTED_LPF_CUTOFF_HZ   1
 
 /**
- * Attenuation should be between 1 to 256.
+ * 衰减应在1到256之间。
  *
  * f0 = fs / 2*pi*attenuation.
  * attenuation = fs / 2*pi*f0
@@ -77,108 +63,109 @@
 #define PM_BAT_IIR_LPF_ATTENUATION (int)(ADC_SAMPLING_FREQ / (int)(2 * 3.1415f * PM_BAT_WANTED_LPF_CUTOFF_HZ))
 #define PM_BAT_IIR_LPF_ATT_FACTOR  (int)((1<<PM_BAT_IIR_SHIFT) / PM_BAT_IIR_LPF_ATTENUATION)
 
-
-
-// #define  PM_ADC1_PIN 5
-#define  PM_ADC1_PIN 2
-
+// ADC1引脚编号
+#define PM_ADC1_PIN 2
 
 typedef enum
 {
-  battery,
-  charging,
-  charged,
-  lowPower,
-  shutDown,
+    battery,      // 电池状态
+    charging,     // 充电中
+    charged,      // 已充满
+    lowPower,     // 低电量
+    shutDown,     // 关机
 } PMStates;
 
 typedef enum
 {
-  charge100mA,
-  charge300mA,
-  charge500mA,
-  chargeMax,
+    charge100mA,  // 充电电流100mA
+    charge300mA,  // 充电电流300mA
+    charge500mA,  // 充电电流500mA
+    chargeMax,    // 最大充电电流
 } PMChargeStates;
 
 typedef enum
 {
-  USBNone,
-  USB500mA,
-  USBWallAdapter,
+    USBNone,         // 无USB连接
+    USB500mA,        // USB电流为500mA
+    USBWallAdapter,  // 使用USB壁挂适配器
 } PMUSBPower;
 
+// 初始化电源管理
 void pmInit(void);
 
+// 测试电源管理是否已初始化
 bool pmTest(void);
 
 /**
- * Power management task
+ * 电源管理任务
  */
 void pmTask(void *param);
 
+// 设置充电状态
 void pmSetChargeState(PMChargeStates chgState);
 // void pmSyslinkUpdate(SyslinkPacket *slp);
 
 /**
- * Returns the battery voltage i volts as a float
+ * 返回电池电压，以浮点数表示（单位：V）
  */
 float pmGetBatteryVoltage(void);
 
 /**
- * Returns the min battery voltage i volts as a float
+ * 返回最小电池电压，以浮点数表示（单位：V）
  */
 float pmGetBatteryVoltageMin(void);
 
 /**
- * Returns the max battery voltage i volts as a float
+ * 返回最大电池电压，以浮点数表示（单位：V）
  */
 float pmGetBatteryVoltageMax(void);
 
 /**
- * Updates and calculates battery values.
- * Should be called for every new adcValues sample.
+ * 更新和计算电池值。
+ * 每次获取新的ADC采样值后都应调用此函数。
  */
 //void pmBatteryUpdate(AdcGroup* adcValues);
 
 /**
- * Returns true if the battery is below its low capacity threshold for an
- * extended period of time.
+ * 如果电池电量低并持续一段时间，则返回true。
  */
 bool pmIsBatteryLow(void);
 
 /**
- * Returns true if the charger is currently connected
+ * 如果充电器当前连接，则返回true。
  */
 bool pmIsChargerConnected(void);
 
 /**
- * Returns true if the battery is currently charging
+ * 如果电池当前正在充电，则返回true。
  */
 bool pmIsCharging(void);
 
 /**
- * Returns true if the battery is currently in use
+ * 如果电池当前正在放电，则返回true。
  */
 bool pmIsDischarging(void);
 
 /**
- * Enable or disable external battery voltage measuring.
+ * 启用或禁用外部电池电压测量。
  */
 void pmEnableExtBatteryVoltMeasuring(uint8_t pin, float multiplier);
 
 /**
- * Measure an external voltage.
+ * 测量外部电压。
  */
 float pmMeasureExtBatteryVoltage(void);
 
 /**
- * Enable or disable external battery current measuring.
+ * 启用或禁用外部电池电流测量。
  */
 void pmEnableExtBatteryCurrMeasuring(uint8_t pin, float ampPerVolt);
 
 /**
- * Measure an external current.
+ * 测量外部电流。
  */
 float pmMeasureExtBatteryCurrent(void);
+
+// 反初始化电源管理
 void pmDeInit(void);
 #endif /* PM_H_ */
--- ports/esp32/py-drone/drivers/pm/pm_esplane.c
+++ ports/esp32/py-drone/drivers/pm/pm_esplane.c
@@ -1,28 +1,10 @@
 /**
- *    ||          ____  _ __
- * +------+      / __ )(_) /_______________ _____  ___
- * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
- * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
- *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
- *
  * ESP-Drone Firmware
+ * pm.c - 电源管理驱动和函数。
  *
- * Copyright 2019-2020  Espressif Systems (Shanghai)
- * Copyright (C) 2011-2012 Bitcraze AB
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, in version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * pm.c - Power Management driver and functions.
+ * 这些函数用于管理电源状态、电池电压、外部电池电压和电流的测量等。
+ * 电源管理任务 pmTask 是一个后台任务，用于持续监测电池状态并采取相应的行动，
+ * 如充电、低电量关机等。其他函数则用于初始化和获取电池状态等。
  */
 
 #include <string.h>
@@ -41,362 +23,311 @@
 
 #define TAG "PM"
 
-typedef struct _PmSyslinkInfo
-{
-  union
-  {
-    uint8_t flags;
-    struct
-    {
-      uint8_t chg    : 1;
-      uint8_t pgood  : 1;
-      uint8_t unused : 6;
+// 定义了电池和外部电池的相关信息，包括电压、电流等
+typedef struct _PmSyslinkInfo {
+    union {
+        uint8_t flags;
+        struct {
+            uint8_t chg: 1;   // 充电标志
+            uint8_t pgood: 1;   // 电源状态标志
+            uint8_t unused: 6;
+        };
     };
-  };
-  float vBat;
-  float chargeCurrent;
+    float vBat;               // 电池电压
+    float chargeCurrent;      // 充电电流
 #ifdef PM_SYSTLINK_INLCUDE_TEMP
-  float temp;
+    float temp;               // 温度
 #endif
 }__attribute__((packed)) PmSyslinkInfo;
 
-static float     batteryVoltage;
-static uint16_t  batteryVoltageMV;
-static float     batteryVoltageMin = 6.0;
-static float     batteryVoltageMax = 0.0;
+static float batteryVoltage;         // 电池电压
+static uint16_t batteryVoltageMV;       // 电池电压（毫伏）
+static float batteryVoltageMin = 6.0;  // 电池电压的最小值，默认初始值为6.0
+static float batteryVoltageMax = 0.0;  // 电池电压的最大值，默认初始值为0.0
 
-static float     extBatteryVoltage;
-static uint16_t  extBatteryVoltageMV;
-static uint16_t extBatVoltDeckPin;
-static bool      isExtBatVoltDeckPinSet = false;
-static float     extBatVoltMultiplier;
-static float     extBatteryCurrent;
-static uint16_t extBatCurrDeckPin;
-static bool      isExtBatCurrDeckPinSet = false;
-static float     extBatCurrAmpPerVolt;
+static float extBatteryVoltage;      // 外部电池电压
+static uint16_t extBatteryVoltageMV;    // 外部电池电压（毫伏）
+static uint16_t extBatVoltDeckPin;        // 外部电池电压测量的引脚编号
+static bool isExtBatVoltDeckPinSet = false;  // 是否设置了外部电池电压测量的引脚
+static float extBatVoltMultiplier;    // 外部电池电压的倍数
+static float extBatteryCurrent;       // 外部电池电流
+static uint16_t extBatCurrDeckPin;        // 外部电池电流测量的引脚编号
+static bool isExtBatCurrDeckPinSet = false;  // 是否设置了外部电池电流测量的引脚
+static float extBatCurrAmpPerVolt;    // 外部电池电流的每伏特安倍数
 
 #ifdef PM_SYSTLINK_INLCUDE_TEMP
-// nRF51 internal temp
-static float    temp;
+// nRF51内部温度
+static float    temp;               // 温度
 #endif
 
-static uint32_t batteryLowTimeStamp;
-static uint32_t batteryCriticalLowTimeStamp;
-static bool isInit;
-static PMStates pmState;
-static PmSyslinkInfo pmSyslinkInfo;
+static uint32_t batteryLowTimeStamp;         // 电池电量低的时间戳
+static uint32_t batteryCriticalLowTimeStamp;  // 电池电量临界低的时间戳
+static bool isInit;                          // 电源管理是否已初始化的标志
+static PMStates pmState;                     // 电源状态
+static PmSyslinkInfo pmSyslinkInfo;           // 电源系统连接信息
 
-static uint8_t batteryLevel;
-static bool isLowpower;
-static void pmSetBatteryVoltage(float voltage);
+static uint8_t batteryLevel;                  // 电池电量级别
+static bool isLowpower;                      // 是否低电量
 
 const static float bat671723HS25C[10] =
-{
-  3.00, // 00%
-  3.78, // 10%
-  3.83, // 20%
-  3.87, // 30%
-  3.89, // 40%
-  3.92, // 50%
-  3.96, // 60%
-  4.00, // 70%
-  4.04, // 80%
-  4.10  // 90%
-};
+        {
+                3.00, // 00%
+                3.78, // 10%
+                3.83, // 20%
+                3.87, // 30%
+                3.89, // 40%
+                3.92, // 50%
+                3.96, // 60%
+                4.00, // 70%
+                4.04, // 80%
+                4.10  // 90%
+        };
 
-bool pmTest(void)
-{
-  return isInit;
+// 测试电源管理是否已初始化
+bool pmTest(void) {
+    return isInit;
 }
 
 /**
- * Sets the battery voltage and its min and max values
+ * 设置电池电压以及其最小和最大值
  */
-
-static void pmSetBatteryVoltage(float voltage)
-{
-  batteryVoltage = voltage;
-  batteryVoltageMV = (uint16_t)(voltage * 1000);
-  if (batteryVoltageMax < voltage)
-  {
-    batteryVoltageMax = voltage;
-  }
-  if (batteryVoltageMin > voltage)
-  {
-    batteryVoltageMin = voltage;
-  }
+static void pmSetBatteryVoltage(float voltage) {
+    batteryVoltage = voltage;
+    batteryVoltageMV = (uint16_t)(voltage * 1000);
+    if (batteryVoltageMax < voltage) {
+        batteryVoltageMax = voltage;
+    }
+    if (batteryVoltageMin > voltage) {
+        batteryVoltageMin = voltage;
+    }
 }
 
 /**
- * Shutdown system
+ * 关闭系统
  */
-static void pmSystemShutdown(void)
-{
+static void pmSystemShutdown(void) {
 #ifdef ACTIVATE_AUTO_SHUTDOWN
-//TODO: Implement syslink call to shutdown
+    //TODO: 实现系统关机的syslink调用
 #endif
 }
 
 /**
- * Returns a number from 0 to 9 where 0 is completely discharged
- * and 9 is 90% charged.
+ * 根据电池电压返回一个从0到9的数字，其中0表示完全放电，9表示90%充电。
  */
-static int32_t pmBatteryChargeFromVoltage(float voltage)
-{
-  int charge = 0;
+static int32_t pmBatteryChargeFromVoltage(float voltage) {
+    int charge = 0;
 
-  if (voltage < bat671723HS25C[0])
-  {
-    return 0;
-  }
-  if (voltage > bat671723HS25C[9])
-  {
-    return 9;
-  }
-  while (voltage >  bat671723HS25C[charge])
-  {
-    charge++;
-  }
+    if (voltage < bat671723HS25C[0]) {
+        return 0;
+    }
+    if (voltage > bat671723HS25C[9]) {
+        return 9;
+    }
+    while (voltage > bat671723HS25C[charge]) {
+        charge++;
+    }
 
-  return charge;
+    return charge;
 }
 
-float pmGetBatteryVoltage(void)
-{
-  return batteryVoltage;
+// 获取电池电压（单位：V）
+float pmGetBatteryVoltage(void) {
+    return batteryVoltage;
 }
 
-float pmGetBatteryVoltageMin(void)
-{
-  return batteryVoltageMin;
+// 获取电池电压的最小值（单位：V）
+float pmGetBatteryVoltageMin(void) {
+    return batteryVoltageMin;
 }
 
-float pmGetBatteryVoltageMax(void)
-{
-  return batteryVoltageMax;
+// 获取电池电压的最大值（单位：V）
+float pmGetBatteryVoltageMax(void) {
+    return batteryVoltageMax;
 }
 
-// void pmSyslinkUpdate(SyslinkPacket *slp)
-// {
-  // if (slp->type == SYSLINK_PM_BATTERY_STATE) {
-    // memcpy(&pmSyslinkInfo, &slp->data[0], sizeof(pmSyslinkInfo));
-    // pmSetBatteryVoltage(pmSyslinkInfo.vBat);
-// #ifdef PM_SYSTLINK_INLCUDE_TEMP
-    // temp = pmSyslinkInfo.temp;
-// #endif
-  // }
-// }
-
-void pmSetChargeState(PMChargeStates chgState)
-{
-  // TODO: Send syslink packafe with charge state
+// 设置充电状态
+void pmSetChargeState(PMChargeStates chgState) {
+    // TODO: 发送带有充电状态的syslink包
 }
 
-PMStates pmUpdateState()
-{
-  PMStates state;
-  bool isCharging = pmSyslinkInfo.chg;
-  bool isPgood = pmSyslinkInfo.pgood;
-  uint32_t batteryLowTime;
+// 更新电源状态
+PMStates pmUpdateState() {
+    PMStates state;
+    bool isCharging = pmSyslinkInfo.chg;
+    bool isPgood = pmSyslinkInfo.pgood;
+    uint32_t batteryLowTime;
 
-  batteryLowTime = xTaskGetTickCount() - batteryLowTimeStamp;
+    batteryLowTime = xTaskGetTickCount() - batteryLowTimeStamp;
 
-  if (isPgood && !isCharging)
-  {
-    state = charged;
-  }
-  else if (isPgood && isCharging)
-  {
-    state = charging;
-  }
-  else if (!isPgood && !isCharging && (batteryLowTime > PM_BAT_LOW_TIMEOUT))
-  {
-    state = lowPower;
-  }
-  else
-  {
-    state = battery;
-  }
+    if (isPgood && !isCharging) {
+        state = charged;
+    } else if (isPgood && isCharging) {
+        state = charging;
+    } else if (!isPgood && !isCharging && (batteryLowTime > PM_BAT_LOW_TIMEOUT)) {
+        state = lowPower;
+    } else {
+        state = battery;
+    }
 
-  return state;
+    return state;
 }
 
-void pmEnableExtBatteryCurrMeasuring(uint8_t pin, float ampPerVolt)
-{
-  extBatCurrDeckPin = pin;
-  isExtBatCurrDeckPinSet = true;
-  extBatCurrAmpPerVolt = ampPerVolt;
+// 启用外部电池电流测量
+void pmEnableExtBatteryCurrMeasuring(uint8_t pin, float ampPerVolt) {
+    extBatCurrDeckPin = pin;
+    isExtBatCurrDeckPinSet = true;
+    extBatCurrAmpPerVolt = ampPerVolt;
 }
 
-float pmMeasureExtBatteryCurrent(void)
-{
-  float current;
+// 测量外部电池电流
+float pmMeasureExtBatteryCurrent(void) {
+    float current;
 
-  if (isExtBatCurrDeckPinSet)
-  {
-    current = analogReadVoltage(extBatCurrDeckPin) * extBatCurrAmpPerVolt;
-  }
-  else
-  {
-    current = 0.0;
-  }
+    if (isExtBatCurrDeckPinSet) {
+        current = analogReadVoltage(extBatCurrDeckPin) * extBatCurrAmpPerVolt;
+    } else {
+        current = 0.0;
+    }
 
-  return current;
+    return current;
 }
 
-void pmEnableExtBatteryVoltMeasuring(uint8_t pin, float multiplier)
-{
-  extBatVoltDeckPin = pin;
-  isExtBatVoltDeckPinSet = true;
-  extBatVoltMultiplier = multiplier;
+// 启用外部电池电压测量
+void pmEnableExtBatteryVoltMeasuring(uint8_t pin, float multiplier) {
+    extBatVoltDeckPin = pin;
+    isExtBatVoltDeckPinSet = true;
+    extBatVoltMultiplier = multiplier;
 }
 
-float pmMeasureExtBatteryVoltage(void)
-{
-  float voltage;
+// 测量外部电池电压
+float pmMeasureExtBatteryVoltage(void) {
+    float voltage;
 
-  if (isExtBatVoltDeckPinSet)
-  {
-    voltage = analogReadVoltage(extBatVoltDeckPin) * extBatVoltMultiplier;
-  }
-  else
-  {
-    voltage = 0.0;
-  }
-
-  return voltage;
+    if (isExtBatVoltDeckPinSet) {
+        voltage = analogReadVoltage(extBatVoltDeckPin) * extBatVoltMultiplier;
+    } else {
+        voltage = 0.0;
+    }
+    return voltage;
 }
 
+// 如果电池电量低则返回true
 bool pmIsBatteryLow(void) {
-  return (pmState == lowPower);
+    return (pmState == lowPower);
 }
 
+// 如果充电器当前连接则返回true
 bool pmIsChargerConnected(void) {
-  return (pmState == charging) || (pmState == charged);
+    return (pmState == charging) || (pmState == charged);
 }
 
+// 如果电池当前正在充电则返回true
 bool pmIsCharging(void) {
-  return (pmState == charging);
-}
-// return true if battery discharging
-bool pmIsDischarging(void)
-{
-  PMStates pmState;
-  pmState = pmUpdateState();
-  return (pmState == lowPower) || (pmState == battery);
+    return (pmState == charging);
 }
 
-void pmTask(void *param)
-{
-	PMStates pmStateOld = battery;
-	uint32_t tickCount = 0;
-	uint32_t batteryCriticalLowTime = 0;
-	#ifdef configUSE_APPLICATION_TASK_TAG
-	#if configUSE_APPLICATION_TASK_TAG == 1
-	vTaskSetApplicationTaskTag(0, (void *)TASK_PM_ID_NBR);
-	#endif
-	#endif
-
-	tickCount = xTaskGetTickCount();
-	batteryLowTimeStamp = tickCount;
-	batteryCriticalLowTimeStamp = tickCount;
-	pmSetChargeState(charge300mA);
-	//systemWaitStart();
-
-	while (1) {
-		vTaskDelay(M2T(100));
-		extBatteryVoltage = pmMeasureExtBatteryVoltage();
-		extBatteryVoltageMV = (uint16_t)(extBatteryVoltage * 1000);
-		extBatteryCurrent = pmMeasureExtBatteryCurrent();
-		pmSetBatteryVoltage(extBatteryVoltage);
-		batteryLevel = pmBatteryChargeFromVoltage(pmGetBatteryVoltage()) * 10;
-
-		tickCount = xTaskGetTickCount();
-
-		if (pmGetBatteryVoltage() > PM_BAT_LOW_VOLTAGE)
-		{
-			batteryLowTimeStamp = tickCount;
-		}
-		if (pmGetBatteryVoltage() > PM_BAT_CRITICAL_LOW_VOLTAGE)
-		{
-			batteryCriticalLowTimeStamp = tickCount;
-		}
-
-		pmState = pmUpdateState();
-		// ESP_LOGI(TAG,"batteryLevel=%u extBatteryVoltageMV=%u ,pmState:%d\n", batteryLevel, extBatteryVoltageMV,pmState);
-		if (pmState != pmStateOld)
-		{
-			// Actions on state change
-			switch (pmState)
-			{
-				case charged:
-					//ledseqStop(CHG_LED,seq_charging);
-					//ledseqRun(CHG_LED, seq_charged);
-				break;
-				case charging:
-					isLowpower = false;
-
-					if(getIsCalibrated())
-					{
-						ledseqRun(SYS_LED, seq_calibrated);
-					}else{
-						ledseqRun(SYS_LED, seq_alive);				
-					}
-
-				break;
-
-				case lowPower:
-					
-					batteryCriticalLowTime = tickCount - batteryCriticalLowTimeStamp;
-					if (batteryCriticalLowTime > PM_BAT_CRITICAL_LOW_TIMEOUT)
-					{
-						pmSystemShutdown();
-					}
-	
-					ledseqStop(SYS_LED, seq_alive);
-					ledseqStop(SYS_LED, seq_calibrated);
-					ledseqRun(LOWBAT_LED, seq_lowbat);
-				break;
-				case battery:
-					//ledseqStop(CHG_LED, seq_charging);
-					//ledseqRun(CHG_LED, seq_charged);
-				break;
-				default:
-				//systemSetCanFly(true);
-				break;
-			}
-			pmStateOld = pmState;
-		}
-	}
-}
-bool getIsLowpower(void)
-{
-	return isLowpower;
+// 如果电池正在放电则返回true
+bool pmIsDischarging(void) {
+    PMStates pmState;
+    pmState = pmUpdateState();
+    return (pmState == lowPower) || (pmState == battery);
 }
 
-void pmInit(void)
-{
-	if(isInit) {
-		return;
-	}
-	adcInit();
+// 电源管理任务
+void pmTask(void *param) {
+    PMStates pmStateOld = battery;
+    uint32_t tickCount = 0;
+    uint32_t batteryCriticalLowTime = 0;
 
-    pmEnableExtBatteryVoltMeasuring(PM_ADC1_PIN, 5); // ADC1 PIN is fixed to ADC channel
+#ifdef configUSE_APPLICATION_TASK_TAG
+#if configUSE_APPLICATION_TASK_TAG == 1
+    vTaskSetApplicationTaskTag(0, (void *)TASK_PM_ID_NBR);
+#endif
+#endif
+
+    tickCount = xTaskGetTickCount();
+    batteryLowTimeStamp = tickCount;
+    batteryCriticalLowTimeStamp = tickCount;
+    pmSetChargeState(charge300mA);
+    // systemWaitStart();
+
+    while (1) {
+        vTaskDelay(M2T(100));
+        extBatteryVoltage = pmMeasureExtBatteryVoltage();
+        extBatteryVoltageMV = (uint16_t)(extBatteryVoltage * 1000);
+        extBatteryCurrent = pmMeasureExtBatteryCurrent();
+        pmSetBatteryVoltage(extBatteryVoltage);
+        batteryLevel = pmBatteryChargeFromVoltage(pmGetBatteryVoltage()) * 10;
+
+        tickCount = xTaskGetTickCount();
+
+        if (pmGetBatteryVoltage() > PM_BAT_LOW_VOLTAGE) {
+            batteryLowTimeStamp = tickCount;
+        }
+        if (pmGetBatteryVoltage() > PM_BAT_CRITICAL_LOW_VOLTAGE) {
+            batteryCriticalLowTimeStamp = tickCount;
+        }
+
+        pmState = pmUpdateState();
+        // ESP_LOGI(TAG,"batteryLevel=%u extBatteryVoltageMV=%u ,pmState:%d\n", batteryLevel, extBatteryVoltageMV,pmState);
+        if (pmState != pmStateOld) {
+            // 状态变化时的操作
+            switch (pmState) {
+                case charged:
+                    //ledseqStop(CHG_LED,seq_charging);
+                    //ledseqRun(CHG_LED, seq_charged);
+                    break;
+                case charging:
+                    isLowpower = false;
+                    if (getIsCalibrated()) {
+                        ledseqRun(SYS_LED, seq_calibrated);
+                    } else {
+                        ledseqRun(SYS_LED, seq_alive);
+                    }
+                    break;
+                case lowPower:
+                    batteryCriticalLowTime = tickCount - batteryCriticalLowTimeStamp;
+                    if (batteryCriticalLowTime > PM_BAT_CRITICAL_LOW_TIMEOUT) {
+                        pmSystemShutdown();
+                    }
+                    ledseqStop(SYS_LED, seq_alive);
+                    ledseqStop(SYS_LED, seq_calibrated);
+                    ledseqRun(LOWBAT_LED, seq_lowbat);
+                    break;
+                case battery:
+                    //ledseqStop(CHG_LED, seq_charging);
+                    //ledseqRun(CHG_LED, seq_charged);
+                    break;
+                default:
+                    //systemSetCanFly(true);
+                    break;
+            }
+            pmStateOld = pmState;
+        }
+    }
+}
+
+bool getIsLowpower(void) {
+    return isLowpower;
+}
+
+void pmInit(void) {
+    if (isInit) {
+        return;
+    }
+    adcInit();
+
+    pmEnableExtBatteryVoltMeasuring(PM_ADC1_PIN, 5); // ADC1引脚固定为ADC通道
 
     pmSyslinkInfo.pgood = false;
     pmSyslinkInfo.chg = false;
     pmSyslinkInfo.vBat = 3.7f;
     pmSetBatteryVoltage(pmSyslinkInfo.vBat);
 
-	// xTaskCreate(pmTask, "pmTask", PM_TASK_STACKSIZE, NULL, PM_TASK_PRI, NULL);
-
+    // xTaskCreate(pmTask, "pmTask", PM_TASK_STACKSIZE, NULL, PM_TASK_PRI, NULL);
     isInit = true;
 }
 
-void pmDeInit(void)
-{
-	
-	isInit = false;
-	
+void pmDeInit(void) {
+    isInit = false;
 }
-
